8b6db20f5e4f85bff6d46ca560bd582f0daa1c3b
Sarkis Peha <sarkispeha@gmail.com>
2024-07-19 16:51:07 -0600

    react query ISS location

--- src/components/Map/HqMap.tsx
+++ src/components/Map/HqMap.tsx
@@ -4,9 +4,20 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { useRef, useState, useEffect } from "react";
 import Map, { GeolocateControl } from "react-map-gl";
 import type { MapRef } from "react-map-gl";
+import { useQuery } from "@tanstack/react-query";
 
 // for reference https://github.com/visgl/react-map-gl/tree/master?tab=readme-ov-file
 // https://www.youtube.com/watch?v=er2YwsForF0
+
+const getIssPosition = async () => {
+  const response = await fetch(
+    "https://api.wheretheiss.at/v1/satellites/25544",
+    { method: "GET" }
+  );
+  const data = await response.json();
+  await console.log("GETTING ISS", data);
+  return { latitude: data.latitude, longitude: data.longitude };
+};
 
 export default function MapboxMap() {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
@@ -16,7 +27,14 @@ export default function MapboxMap() {
   const mapRef = useRef<MapRef | null>(null);
 
   const [isMapReady, setIsMapReady] = useState<boolean>(false);
+  const [isRefetching, setIsRefetching] = useState<boolean>(false);
   const [viewport, setViewport] = useState({});
+  const query = useQuery({
+    queryKey: ["issPosition"],
+    queryFn: getIssPosition,
+    refetchInterval: isRefetching ? 6000 : false,
+  });
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setViewport({
@@ -33,12 +51,25 @@ export default function MapboxMap() {
       center: [-114.58030531385444, 46.63475261852412],
     });
   };
+
+  const toggleTrackIssHandler = async () => {
+    setIsRefetching(!isRefetching);
+  };
   return (
     <div>
       Should be a map here
       <button onClick={flyToLoloHandler} style={{ border: "1px solid #000" }}>
         Go to Lolo Pass
       </button>
+      <button
+        onClick={toggleTrackIssHandler}
+        style={{ border: "1px solid #000" }}
+      >
+        ISS location
+      </button>
+      <p>
+        {`Latitude: ${query.data?.latitude} Longitude: ${query.data?.longitude}`}
+      </p>
       {isMapReady && (
         <Map
           ref={mapRef}
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { createContext, useState, useContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const AppContext = createContext({
   hello: "world",
 });
+
+const queryClient = new QueryClient();
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
   let [state, setState] = useState({
     hello: "world",
   });
 
-  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={state}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </AppContext.Provider>
+  );
 }
 
 export function useAppContext() {
