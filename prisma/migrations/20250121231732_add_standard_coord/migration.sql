3789a3754ca1b52397d7644b693d208afe841a17
sarkispeha <sarkispeha@gmail.com>
2025-01-22 11:12:36 -0700

    postgres migrations

---
+++ prisma/migrations/20250121231732_add_standard_coord/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "StandardCoord" (
+    "id" TEXT NOT NULL,
+    "lng" DOUBLE PRECISION NOT NULL,
+    "lat" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "StandardCoord_pkey" PRIMARY KEY ("id")
+);
