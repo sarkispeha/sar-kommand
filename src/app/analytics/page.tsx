1967fbb1f7c02562710d91fe4ff4aec1de530cd9
sarkispeha <sarkispeha@gmail.com>
2025-01-29 10:31:01 -0700

    add analytics page

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,11 @@
+export default function AnalyticsPage() {
+  return (
+    <div>
+      <h1>Analytics</h1>
+      <p>
+        This is the analytics page, it will have a callout table, number of
+        hours, prevous callout maps etc
+      </p>
+    </div>
+  );
+}
