1ca6c9ed12e2d42351afb223bfccc00ec920454e
sarkispeha <sarkispeha@gmail.com>
2025-03-05 19:48:20 -0700

    clean up unused ISS

--- src/components/Map/MapboxMap.tsx
+++ src/components/Map/MapboxMap.tsx
@@ -2,59 +2,27 @@
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useRef, useState, useEffect } from "react";
-import Map, { GeolocateControl, Layer, Marker, Source } from "react-map-gl";
-import type { CircleLayer, LineLayer, MapRef } from "react-map-gl";
-import type { Feature, FeatureCollection, LineString, Position } from "geojson";
+import Map, { GeolocateControl } from "react-map-gl";
+import type { MapRef } from "react-map-gl";
 import { GeoJsonPosition } from "@/data/Geo";
 import type { MemberCoord } from "@prisma/client";
 import { MemberData } from "@/data/Member";
 import { MemberMarker } from "./MemberMarker";
 
-// for reference https://github.com/visgl/react-map-gl/tree/master?tab=readme-ov-file
-// https://www.youtube.com/watch?v=er2YwsForF0
-
-interface IssDataResponse {
-  data: {
-    latitude: number;
-    longitude: number;
-  };
-  isLoading: boolean;
-}
-
 interface MapboxMapProps {
-  issPosition: GeoJsonPosition | null;
-  isRefetching: boolean;
-  isTrackingIss: boolean;
   isTrailing: boolean;
-  trailingArray: Position[];
   selectedMembers: MemberData[];
   mobileMemberPosition: MemberCoord | null;
 }
 
-export default function MapboxMap({
-  issPosition,
-  isRefetching,
-  isTrackingIss,
-  isTrailing,
-  trailingArray,
-  selectedMembers,
-  mobileMemberPosition,
-}: MapboxMapProps) {
+export default function MapboxMap({ selectedMembers }: MapboxMapProps) {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
   const mapRef = useRef<MapRef | null>(null);
 
   const [isMapReady, setIsMapReady] = useState<boolean>(false);
   const [viewport, setViewport] = useState({});
-  const [isTracking, setIsTracking] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (issPosition && isRefetching && isTrackingIss) {
-      flyToLocation({
-        location: { lat: issPosition.lat, lng: issPosition.lng },
-      });
-    }
-  }, [issPosition]);
-
+  // TODO: put in map util
   const flyToLocation = ({ location }: { location: GeoJsonPosition }) => {
     mapRef.current?.flyTo({
       center: location,
@@ -64,6 +32,7 @@ export default function MapboxMap({
     });
   };
 
+  // set map starting location
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setViewport({
@@ -76,70 +45,8 @@ export default function MapboxMap({
     });
   }, []);
 
-  // const circleJson: FeatureCollection = {
-  //   type: "FeatureCollection",
-  //   features: [
-  //     {
-  //       type: "Feature",
-  //       properties: { circle: "circle" },
-  //       geometry: {
-  //         type: "Point",
-  //         coordinates: [-114.00874412405805, 46.85408927066709],
-  //       },
-  //     },
-  //   ],
-  // };
-
-  // const circleStyle: CircleLayer = {
-  //   id: "point",
-  //   type: "circle",
-  //   paint: {
-  //     "circle-radius": 10,
-  //     "circle-color": "#007cbf",
-  //   },
-  // };
-
-  const lineStyle: LineLayer = {
-    id: "lineLayer",
-    type: "line",
-    source: "lineSource",
-    layout: {
-      "line-join": "round",
-      "line-cap": "round",
-    },
-    paint: {
-      "line-color": "red",
-      "line-width": 10,
-    },
-  };
-  const lineJson: Feature = {
-    type: "Feature",
-    properties: {},
-    geometry: {
-      type: "LineString",
-      coordinates: trailingArray,
-      // [[-114.00874412405805, 46.85408927066709],
-      // [-114.00874412405805, 47.85408927066709],
-      // [-114.00874412405805, 48.85408927066709],
-      // [-114.00874412405805, 49.85408927066709]],
-    },
-  };
-
   return (
     <div>
-      <p style={{ border: "1px solid #000" }}>
-        ISS location Tracking: {isRefetching ? "Enabled" : "Disabled"}
-      </p>
-      <p
-        onClick={() => {
-          console.log("START TRACK");
-          setIsTracking(!isTracking);
-        }}
-      >
-        Check the map ref
-      </p>
-      <p>DRAW LINE? {isTracking ? "Enabled" : "Disabled"}</p>
-      {/* <p>{`Latitude: ${issPosition?.lat} Longitude: ${issPosition?.lng}`}</p> */}
       {isMapReady && (
         <Map
           ref={mapRef}
@@ -152,20 +59,6 @@ export default function MapboxMap({
             trackUserLocation={true}
             positionOptions={{ enableHighAccuracy: true }}
           />
-          {issPosition && (
-            <Marker
-              longitude={issPosition?.lng}
-              latitude={issPosition?.lat}
-              color="red"
-            />
-          )}
-          {/* {mobileMemberPosition && (
-            <Marker
-              longitude={mobileMemberPosition.lng}
-              latitude={mobileMemberPosition.lat}
-              color="blue"
-            />
-          )} */}
           {selectedMembers &&
             selectedMembers.map((selectedMember) =>
               selectedMember.position ? (
@@ -175,9 +68,6 @@ export default function MapboxMap({
                 />
               ) : null
             )}
-          <Source id="lineSource" type="geojson" data={lineJson}>
-            <Layer {...lineStyle} />
-          </Source>
         </Map>
       )}
     </div>
